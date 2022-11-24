1c108d54f3b1c45fd541ad7f8f673e9900f2374f
mubin028 <93798596+mubin028@users.noreply.github.com>
2022-11-24 15:09:04 -0500

    commit 7

--- 3. Build a Blackjack game/15. Display the sum/index.js
+++ 3. Build a Blackjack game/15. Display the sum/index.js
@@ -5,10 +5,11 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEL = document.querySelector("#sum-el")
 // 2. Store the sum paragraph in a variable called sumEl
 
 function startGame() {
-    // 3. Render the sum on the page using this format -> "Sum: 14"
+    sumEL.textContent= "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
