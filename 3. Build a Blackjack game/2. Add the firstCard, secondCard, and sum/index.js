969bb251043d76a44994b1cf88d53c5b1c0ae1c1
mubin028 <93798596+mubin028@users.noreply.github.com>
2022-11-24 14:45:54 -0500

    commit 6 lmao

--- 3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
+++ 3. Build a Blackjack game/2. Add the firstCard, secondCard, and sum/index.js
@@ -2,3 +2,10 @@
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
+ let firstCard = Math.floor(Math.random() * 10) + 2
+ let secondCard = Math.floor(Math.random() * 10) + 2
+
+ let sum = firstCard + secondCard
+ console.log(sum)
+
+ 
--- 3. Build a Blackjack game/5. if_else...if_else statement/index.js
+++ 3. Build a Blackjack game/5. if_else...if_else statement/index.js
@@ -1,7 +1,10 @@
 // Check if the person is elegible for a birthday card from the King! (100)
 
 let age = 100
-
-// if less than 100    -> "Not elegible"
-// else if exactly 100 -> "Here is your birthday card from the King!"
-// else                -> "Not elegible, you have already gotten one"
+if (age < 100) {
+    console.log("You are not yet 100 years old")
+} else if (age == 100) {
+    console.log("Here is your birthday card from the King!")
+} else {
+    console.log("Not elegible, you have already gotten one")
+}
