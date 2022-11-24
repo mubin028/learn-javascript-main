ad9bfe39a7b51294d28e9afaf806ad4f739d4568
mubin028 <93798596+mubin028@users.noreply.github.com>
2022-11-24 00:07:10 -0500

    commit 3

--- 2. Practice time - part 1/1. Variables practice/index.js
+++ 2. Practice time - part 1/1. Variables practice/index.js
@@ -1,5 +1,8 @@
 // Create two variables, firstName and lastName
+let firstName = "Mubin"
+let lastName = "Qureshi"
 
+console.log(firstName+" "+lastName)
 // Concatenate the two variables into a third variable called fullName
 
 // Log fullName to the console
--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -1,5 +1,8 @@
 // When the user clicks the purchase button, render out
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
-
+let errorEL = document.getElementById("error");
+function error(){
+    errorEL.innerText = "Something went wrong, please try again";
+}
 
