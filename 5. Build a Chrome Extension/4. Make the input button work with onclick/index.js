53ec23737f2554d21ddfa499115b0d31fbb21ca1
mubin028 <93798596+mubin028@users.noreply.github.com>
2022-12-22 19:57:11 -0500

    chrome extension

--- 5. Build a Chrome Extension/4. Make the input button work with onclick/index.js
+++ 5. Build a Chrome Extension/4. Make the input button work with onclick/index.js
@@ -1,1 +1,6 @@
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+
+let inputBtn =document.getElementById("input-btn")
+inputBtn.addEventListener("click",function() {
+    console.log("button clicked")
+})
--- 5. Build a Chrome Extension/8. Create the myLeads array and inputEl/index.js
+++ 5. Build a Chrome Extension/8. Create the myLeads array and inputEl/index.js
@@ -2,7 +2,12 @@
 // myLeads -> should be assigned to an empty array
 // inputEl -> should be assigned to the text input field
 
-let inputBtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
+
+const inputEl = document.getElementById("input-el")
+let myLeads = []
+
+
 
 inputBtn.addEventListener("click", function() {
     console.log("Button clicked!")
